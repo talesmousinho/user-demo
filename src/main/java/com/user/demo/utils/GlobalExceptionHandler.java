ba80a86b7c818acd8563518217d036104e81de23
t a l e s <tales.mousinho@gmail.com>
2024-01-06 17:58:42 +0000

    Refactor GlobalExceptionHandler to use RestControllerAdvice

--- src/main/java/com/user/demo/utils/GlobalExceptionHandler.java
+++ src/main/java/com/user/demo/utils/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.user.demo.utils;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,46 +12,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request) {
     logger.error("Exception: ", ex);
 
-    Map<String, String> errorMessages = new HashMap<>();
-    ex.getBindingResult().getAllErrors().forEach(error -> errorMessages.put(((FieldError) error).getField(), error.getDefaultMessage()));
-
-    return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
+    return ex.getBindingResult().getFieldErrors().stream()
+        .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<Map<String, String>> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpServletRequest request) {
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public Map<String, String> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpServletRequest request) {
     logger.error("Exception: ", ex);
-    Map<String, String> errorMessages = new HashMap<>();
 
     Throwable rootCause = ex.getMostSpecificCause();
     if (rootCause instanceof InvalidFormatException) {
       InvalidFormatException invalidFormatException = (InvalidFormatException) rootCause;
-      invalidFormatException.getPath().forEach(reference -> errorMessages.put(reference.getFieldName(), "Invalid format"));
+      return invalidFormatException.getPath().stream()
+          .collect(Collectors.toMap(JsonMappingException.Reference::getFieldName, reference -> "Invalid format"));
     } else {
-      // Generic error message for other types of HttpMessageNotReadableException
-      errorMessages.put("error", "Malformed JSON request");
+      return Collections.singletonMap("error", "Malformed JSON request");
     }
-
-    return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(EntityNotFoundException.class)
